f67b075190454f27cab78561040108ae5f5efded
Rachita Upreti <rachita.upretir@lenoni.fi>
2017-08-24 14:21:38 +0200

    hello

---
+++ app/controllers/ContactPageController.js
@@ -0,0 +1,19 @@
+angular.module('ContactPageModule',[])
+.controller('ContactPageController', function($scope) {
+     
+})
+.controller('FirstChildController', function($scope) {
+     
+    $scope.message = 'Welcome to My Corporation';
+     
+})
+.controller('SecondChildController', function($scope) {
+     
+    $scope.message = 'Welcome ';
+     
+})
+.controller('ThirdChildController', function($scope) {
+     
+    $scope.message = 'Welcome ';
+     
+});
---
+++ app/controllers/TietoameistäPageController.js
@@ -0,0 +1,19 @@
+angular.module('TietoameistäPageModule',[])
+.controller('TietoameistäPageController', function($scope) {
+     
+})
+.controller('FirstChildController', function($scope) {
+     
+    $scope.message = 'Welcome to My Corporation';
+     
+})
+.controller('SecondChildController', function($scope) {
+     
+    $scope.message = 'Welcome ';
+     
+})
+.controller('ThirdChildController', function($scope) {
+     
+    $scope.message = 'Welcome ';
+     
+});
